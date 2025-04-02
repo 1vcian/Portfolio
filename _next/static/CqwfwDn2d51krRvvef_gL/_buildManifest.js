a9b1a18ac6d5cd86fb28d2a5170a79bf04265722
Lucian <medrihanlucian@gmail.com>
2025-04-02 14:30:02 +0200

    Updates

--- _next/static/CqwfwDn2d51krRvvef_gL/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/65aa3619a1925c9d.css","static/chunks/867-ad4d9b7d63141e48.js","static/chunks/pages/index-9d0a059d71a4c003.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
