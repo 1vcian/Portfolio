3f6200b10fb4237d9a87eba19a3d5f2b0c3b651e
1vcian <medrihanlucian@gmail.com>
2026-05-18 10:33:17 +0000

    Deploying to gh-pages from @ 1vcian/Portfolio@95f1072a08f3f9a03ee082ee82ce9770165901e4 🚀

---
+++ _next/static/GJim5PxqIIwjFNhY0eLkK/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/65aa3619a1925c9d.css","static/chunks/867-ad4d9b7d63141e48.js","static/chunks/pages/index-57d8421a2cda0b32.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
